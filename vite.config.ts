c719ebd909e6247dbfdb202b0e745b7191e3478a
Akash Lukhi <48802690+akashlukhi@users.noreply.github.com>
2024-01-22 23:18:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import gzipPlugin from "rollup-plugin-gzip";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/Portfolio/",
   plugins: [react()],
