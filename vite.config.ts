4c3476aa65f1c1158e30913e366f40c172fe93d0
Akash Lukhi <48802690+akashlukhi@users.noreply.github.com>
2024-02-04 14:59:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/Portfolio/",
+  publicPath: '/Portfolio/',
   plugins: [react()],
   ssr: {
     noExternal: ['react', 'react-dom'],
